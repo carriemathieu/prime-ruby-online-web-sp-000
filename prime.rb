f06215e79627190b6e8141c6ab12e2bbd13b18f0
Carrie Mathieu <carrieburks@gmail.com>
2020-08-09 23:16:45 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(int)
+  return false if int <= 1
+  return true if int == 2
+  (2..(int-1)).each do |xs| 
+    if int % x==0 
+      return false 
+    else
+      return true
+    end
+  end
+end
