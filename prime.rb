246e7f8f7aa048de4941b6645855874b26bd6ed7
Carrie Mathieu <carrieburks@gmail.com>
2020-08-09 23:18:37 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,10 +3,7 @@ def prime?(int)
   return false if int <= 1
   return true if int == 2
   (2..(int-1)).each do |x| 
-    if int % x==0 
-      return false 
-    else
-      return true
-    end
+    return false if int % x==0 
   end
+  true
 end
